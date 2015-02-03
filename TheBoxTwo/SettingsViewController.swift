79cec9d67be50f0e9fa3e549a3f88bd016001f71
tgulik <tgulik@yahoo-inc.com>
2015-02-02 20:22:51 -0800

    Improvements
    Adding TOS page … I’ve added gesture recognizers but can’t get them to
    respond. Not sure why.

---
+++ TheBoxTwo/SettingsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SettingsViewController.swift
+//  TheBoxTwo
+//
+//  Created by Tomek Gulik on 2/2/15.
+//  Copyright (c) 2015 Tom Gulik. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet var scrollViewer: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        scrollViewer.contentSize = CGSize(width:320, height:1000)
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
