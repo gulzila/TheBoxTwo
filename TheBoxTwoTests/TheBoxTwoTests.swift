3d7a286335445458941652039bf3b57949f3b056
tgulik <tgulik@yahoo-inc.com>
2015-02-02 19:27:24 -0800

    Adding 2nd try of Box homework
    @codepathreview

---
+++ TheBoxTwoTests/TheBoxTwoTests.swift
@@ -0,0 +1,36 @@
+//
+//  TheBoxTwoTests.swift
+//  TheBoxTwoTests
+//
+//  Created by Tomek Gulik on 2/2/15.
+//  Copyright (c) 2015 Tom Gulik. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TheBoxTwoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
